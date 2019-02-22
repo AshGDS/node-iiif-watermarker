fdfd3c45bbfa0ecaef4700379e7fbd6bd874d2d1
AshTNA <ashleyaburnett@gmail.com>
2019-02-22 11:24:35 +0000

    Watermark server should now be fully dynamic and work with any IIIF Image server.:

--- server.js
+++ server.js
@@ -1,60 +1,53 @@
 const express = require('express');
 const app = express();
-const request = require('request');
-const fs = require('fs');
+const request = require('request').defaults({encoding: null});
 const sharp = require('sharp');
 
+// This will set the URL of your IIIF server to http://localhost:8182/iiif/2/
+const IMAGE_API_SERVER = 'http://localhost';
+const IMAGE_API_PORT = 8182;
+const IMAGE_API_PREFIX = 'iiif/2';
 
+const WATERMARKER_PORT = 4343; // Port for this server
 
-// Taken from https://stackoverflow.com/questions/12740659/downloading-images-with-node-js
-let download = function (uri, filename, callback) {
+let download = function (uri, callback) {
 
-    request.head(uri, function (err, res, body) {
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
-
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    request.get(uri, function (err, res, body) {
+        callback(body);
     });
-
 };
 
 app.use(function (req, res, next) {
 
-    // disable CORS
-
+    // Disable CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 app.get('/', (req, res) => {
+
     res.send('Hello');
     console.log('');
 })
 
-// Point your manifest image to this port
-app.listen(4343, (error) => {
-    error ? console.log(error) : console.log('server running');
+app.listen(WATERMARKER_PORT, (error) => {
+
+    error ? console.log(error) : console.log('Watermark server running...');
 })
 
-
-app.get('/iiif/2/:file/:coords/:size/:num/:type', (req, res) => {
+app.get(`/${IMAGE_API_PREFIX}/:file/:coords/:size/:num/:type`, (req, res) => {
 
     let {file, coords, size, num, type} = req.params;
 
-    let date = new Date();
-    let random = date.getTime();
-
-
-    download(`http://localhost:8182/iiif/2/${file}/${coords}/${size}/${num}/${type}`, `${random}-${file}`, () => {
+    download(`${IMAGE_API_SERVER}:${IMAGE_API_PORT}/${IMAGE_API_PREFIX}/${file}/${coords}/${size}/${num}/${type}`, (rawImage) => {
 
         let options = {tile: true};
 
-        sharp(`${random}-${file}`)
+        sharp(rawImage)
             .overlayWith('watermark.png', options)
             .toBuffer()
             .then(data => {
-                console.log('Watermark success');
                 res.send(data);
             })
             .catch(err => {
@@ -64,15 +57,28 @@ app.get('/iiif/2/:file/:coords/:size/:num/:type', (req, res) => {
 
 });
 
-
-// Temporary workaround for sending info.json through this proxy
-
-app.get('/iiif/2/:file/info.json', (req, res) => {
+app.get(`/${IMAGE_API_PREFIX}/:file/info.json`, (req, res) => {
 
     let {file} = req.params;
-
-    res.sendFile(`${__dirname}/${file}/info.json`);
+    send_IIIF_json(res, file);
 });
 
+app.get(`/${IMAGE_API_PREFIX}/:file`, (req, res) => {
+
+    let {file} = req.params;
+    send_IIIF_json(res, file);
+});
+
+let send_IIIF_json = (res, file) => {
+
+    download(`${IMAGE_API_SERVER}:${IMAGE_API_PORT}/${IMAGE_API_PREFIX}/${file}/info.json`, (json) => {
+        let encoded_json = JSON.parse(json);
+
+        // Changes info.json to point to this servers mock IIIF Image server port
+        encoded_json['@id'] = encoded_json['@id'].replace(`:${IMAGE_API_PORT}/`,`:${WATERMARKER_PORT}/`)
+
+        res.send(encoded_json);
+    })
+}
 
 
